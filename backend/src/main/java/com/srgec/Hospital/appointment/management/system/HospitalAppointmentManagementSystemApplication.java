5cb86ddb831d43245fa74be5768a51c22b772aa0
aruna182 <raaviaruna91@gmail.com>
2026-06-26 21:08:37 +0530

    Add files via upload

---
+++ backend/src/main/java/com/srgec/Hospital/appointment/management/system/HospitalAppointmentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.srgec.Hospital.Appointment.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAppointmentManagementSystemApplication.class, args);
+	}
+
+}
